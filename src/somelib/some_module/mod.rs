61b1536245d56bfc9c7444cf4023bf7e8dcb71d9
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-09-03 10:56:40 +0200

    This also works
    readmes in subdirectories of an included lib crate are included in
    rustdoc output.

--- src/somelib/some_module/mod.rs
+++ src/somelib/some_module/mod.rs
@@ -1,7 +1,5 @@
 #![doc = include_str!("readme.md")]
 
-pub mod some_module;
-
 pub fn test() {
-    println!("Hello, lib!");
+    println!("Hello, mod!");
 }
---
+++ src/somelib/some_module/readme.md
@@ -0,0 +1,5 @@
+# Here is some lib-crate module readme
+
+here are some pears:
+
+![pears.jpg](../assets/pears.jpg)
